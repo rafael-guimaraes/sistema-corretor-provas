fba43317d887517c726032dd710b97ed82b26cf1
Gabriel Fileno <gabriel.costa.fileno@gmail.com>
2024-05-02 09:06:33 -0300

    Simulando response e printando request

--- servidor/controle.py
+++ servidor/controle.py
@@ -7,10 +7,16 @@ from modelo.banco import Banco
 
 async def requisicoes(websocket, path):
     async for message in websocket:
-        dado = json.loads(message)
-        
-        json_resposta = ""
-        await websocket.send(json_resposta)
+        print(message)
+        payload = str({"status":200,"body":["Lista1","Lista2"]})
+        a = await websocket.send(payload)
+        print("envio:" + str(a))
+        print("enviado:" + payload)
+        # dado = json.loads(message)
+        # print(dado)
+        # print(message)
+        # json_resposta = ""
+        # await websocket.send(json_resposta)
 
 async def socket():
     async with websockets.serve(requisicoes, "localhost", 5000):
