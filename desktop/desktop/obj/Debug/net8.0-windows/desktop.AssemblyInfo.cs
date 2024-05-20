d06e58b3c47974b551341cea277f5e71479ec7d7
Gabriel Fileno <gabriel.costa.fileno@gmail.com>
2024-05-20 12:11:16 -0300

    monografia: adicionando metodologia kanban

--- desktop/desktop/obj/Debug/net8.0-windows/desktop.AssemblyInfo.cs
+++ desktop/desktop/obj/Debug/net8.0-windows/desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e40d358d917e6b6149ba5ff29650bb1574a983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+806ad564f1036320fdf99b535d4b83e1f62c8b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
